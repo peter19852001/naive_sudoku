0a465c1a1837c5656d907bbcde3af42af385a550
Peter <lylo@cse.cuhk.edu.hk>
2013-06-20 12:57:42 +0800

    Basically done.

--- sudoku.lisp
+++ sudoku.lisp
@@ -20,6 +20,7 @@
 (defun singleton (n) (ash 1 n))
 (defun intersect-set (s1 s2) (logand s1 s2))
 (defun union-set (s1 s2) (logior s1 s2))
+(defparameter *empty-set* 0)
 (defparameter *universal-set* #b1111111110)
 (defun complement-set (s) (logand *universal-set* (lognot s)))
 (defun difference-set (s1 s2) (intersect-set s1 (complement-set s2)))
@@ -27,9 +28,27 @@
   (let ((s (make-array 10 :initial-element nil)))
     (dotimes (i 9 s)
       (setf (aref s (1+ i)) (singleton (1+ i))))))
+(defun subset? (s1 s2) (= s1 (intersect-set s1 s2)))
 (defun singleton? (s) (find s *singletons*))
 (defun singleton-num (s) (position s *singletons*))
 
+(defmacro do-singletons (v &body body)
+  (let ((i (gensym)))
+    `(loop for ,i from 1 to 9
+	do (let ((,v (aref *singletons* ,i)))
+	     ,@body))))
+(defmacro do-singletons-of ((v s) &body body)
+  (let ((se (gensym)))
+    `(let ((,se ,s))
+       (do-singletons ,v
+	 (when (subset? ,v ,se)
+	   ,@body)))))
+
+(defun set-size (s)
+  (let ((n 0))
+    (do-singletons-of (x s)
+      (incf n))
+    n))
 ;;; a board is (internally) represented as a 9x9 2D array of small sets
 (defun empty-board ()
   (make-array '(9 9) :initial-element *universal-set*))
@@ -71,13 +90,44 @@
   (if b (print-raw-board b) (format t "Invalid Board!~%")))
 ;;
 (defun valid-board (b)
-  ;; returns two values: is-valid and is-complete
-  (let ((is-complete t))
-    (dotimes (i 9)
+  ;; returns nil if invalid. Returns 'valid if valid but not complete, and returns 'complete if valid and complete
+  (let ((is-complete t)
+	(c-set *empty-set*))
+    (labels ((prepare-check () (setf c-set *empty-set*))
+	     (entry-not-ok? (s)
+	       (and (singleton? s)
+		    (or (subset? s c-set)
+			(progn (setf c-set (union-set s c-set))
+			       nil))))
+	     (permutation1-9? () (= c-set *universal-set*)))
+      ;; rows
+      (dotimes (i 9)
+	(prepare-check)
+	(dotimes (j 9)
+	  (if (entry-not-ok? (aref b i j))
+	      (return-from valid-board nil)))
+	(unless (permutation1-9?) (setf is-complete nil)))
+      ;; columns
       (dotimes (j 9)
-	(
+	(prepare-check)
+	(dotimes (i 9)
+	  (if (entry-not-ok? (aref b i j))
+	      (return-from valid-board nil)))
+	(unless (permutation1-9?) (setf is-complete nil)))
+      ;; squares
+      (dolist (sq-x '(0 3 6))
+	(dolist (sq-y '(0 3 6))
+	  (prepare-check)
+	  (dotimes (i 3)
+	    (dotimes (j 3)
+	      (if (entry-not-ok? (aref b (+ sq-x i) (+ sq-y j)))
+		  (return-from valid-board nil))))
+	  (unless (permutation1-9?) (setf is-complete nil))))
+      ;;
+      (if is-complete 'complete 'valid))))
+
 ;;
-(defparameter *test*
+(defparameter *test-easy*
   '(0 0 0  3 0 5  0 6 0
     3 0 7  6 0 4  0 0 0
     0 8 0  0 0 9  3 0 4
@@ -89,8 +139,48 @@
     2 0 5  8 0 0  0 9 0
     0 0 0  7 0 3  2 0 5
     0 9 0  4 0 2  0 0 0))
-(defparameter *test-b* (list-to-board *test*))
-;; (print-board *test-b*)
+(defparameter *test-medium*
+  '(0 0 0  0 0 0  1 3 8
+    5 9 0  0 0 1  0 7 0
+    3 0 0  0 0 4  0 0 0
+
+    0 0 0  9 4 0  3 8 1
+    4 0 0  0 1 0  0 0 7
+    1 5 8  0 7 3  0 0 0
+
+    0 0 0  6 0 0  0 0 9
+    0 7 0  1 0 0  0 5 3
+    8 1 3  0 0 0  0 0 0))
+(defparameter *test-hard*
+  '(0 7 0  0 8 0  0 0 2
+    8 0 1  0 0 0  0 9 0
+    0 2 0  0 9 5  0 0 1
+
+    4 0 0  0 0 0  6 0 0
+    0 0 0  2 1 9  0 0 0
+    0 0 9  0 0 0  0 0 3
+
+    1 0 0  5 6 0  0 2 0
+    0 6 0  0 0 0  1 0 7
+    5 0 0  0 4 0  0 3 0))
+(defparameter *test-evil*
+  '(0 8 0  0 0 5  0 0 0
+    1 0 2  0 7 0  0 4 0
+    0 0 3  0 0 0  6 0 0
+
+    0 0 8  0 0 7  9 5 0
+    0 0 0  0 1 0  0 0 0
+    0 6 1  3 0 0  8 0 0
+
+    0 0 4  0 0 0  1 0 0
+    0 1 0  0 6 0  2 0 8
+    0 0 0  5 0 0  0 9 0))
+
+(defparameter *test-easy-b* (list-to-board *test-easy*))
+(defparameter *test-medium-b* (list-to-board *test-medium*))
+(defparameter *test-hard-b* (list-to-board *test-hard*))
+(defparameter *test-evil-b* (list-to-board *test-evil*))
+;; (print-board *test-easy-b*)
 ; +===+===+===++===+===+===++===+===+===+
 ; |   |   |   || 3 |   | 5 ||   | 6 |   |
 ; +---+---+---++---+---+---++---+---+---+
@@ -154,4 +244,51 @@
 	    (push (cons i j) cells-to-do))))
     (propagate-constraints b cells-to-do)))
 ;;
-(defparameter *test-b-p* (init-propagate-board (copy-board *test-b*)))
+(defparameter *test-easy-b-p* (init-propagate-board (copy-board *test-easy-b*)))
+(defparameter *test-medium-b-p* (init-propagate-board (copy-board *test-medium-b*)))
+(defparameter *test-hard-b-p* (init-propagate-board (copy-board *test-hard-b*)))
+(defparameter *test-evil-b-p* (init-propagate-board (copy-board *test-evil-b*)))
+;;
+(defun cell-order-to-try (b)
+  ;; try those with less choices first, exclude those singletons
+  (let ((v (make-array 10 :initial-element nil))
+	(r nil))
+    (dotimes (i 9)
+      (dotimes (j 9)
+	(let ((c-size (set-size (aref b i j))))
+	  (if (> c-size 1) (push (cons i j) (aref v c-size))))))
+    ;; collect them
+    (loop for i from 9 downto 2
+       do (setf r (nconc (aref v i) r)))
+    (values r v)))
+;; The depth first search part
+(defun try-cells (b cells-to-try)
+  (if (null cells-to-try)
+      (if (eq 'complete (valid-board b)) b nil)
+      (let* ((cell (car cells-to-try))
+	     (i (car cell))
+	     (j (cdr cell))
+	     (s (aref b i j)))
+	(if (singleton? s)
+	    (try-cells b (cdr cells-to-try))
+	    (do-singletons-of (x s)
+	      (let* ((nb (put-on-board b i j x))
+		     (res-b (and nb (try-cells nb (cdr cells-to-try)))))
+		(if res-b (return-from try-cells res-b))))))))
+;;
+(defun solve-sudoku (b)
+  ;; b is just a board, possibly without initial propagation
+  (let ((nb (init-propagate-board (copy-board b))))
+    (try-cells nb (cell-order-to-try nb))))
+
+
+;; wrapper, from list of initial configuration to solved board, if possible
+(defun solve-sudoku-list (ns)
+  (let ((b (list-to-board ns)))
+    (format t "Puzzle:~%")
+    (print-board b)
+    (format t "~%Try to solve.~%")
+    (print-board (solve-sudoku b))))
+;;;
+
+
